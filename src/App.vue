cc636cb3b519c37d4c9e0e04df6f5ddc0594d46f
Innovanito <khg05311@naver.com>
2021-10-21 16:46:56 +0900

    import oneroom data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div v-if="모달창열렸니" class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p> 상세페이지 내용임</p>
+      <button @click="모달창열렸니= false">닫기</button>
+    </div>    
+  </div>
+
+  <div class="menu">
+    <a v-for="메뉴 in 메뉴들" :key="메뉴"> {{ 메뉴}} </a>
+  </div>
+
+
+
+  <div v-for="(원룸, i) in 원룸들" :key="원룸" >
+    <img :src="원룸들[i].image" class="room-img" alt="">
+    <h4 @click="모달창열렸니= true">
+      {{ 원룸들[i].title}}
+    </h4>
+    <p> {{ 원룸들[i].price}} 원</p>
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/oneroom' 
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {  
+      원룸들 : data,
+      모달창열렸니: false,    
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.room-img {
+  width: 80%;
+  margin-top: 40px;
+}
+body{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
